f76a0fe959f0a82262dd0f1fb191b03dbee1d090
Aleksa-Miks <aleksa.m@coinis.com>
2025-10-31 13:59:39 +0100

    [frontend] configure eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,35 @@
-import { defineConfig } from "vite";
+import { defineConfig, type PluginOption } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+const shouldUseChecker = true; // or any condition you want
+
+export default defineConfig(async () => {
+  const plugins: PluginOption[] = [react(), tailwindcss()];
+
+  if (shouldUseChecker) {
+    const { checker } = await import("vite-plugin-checker");
+
+    const checkerPlugin = checker({
+      typescript: {
+        tsconfigPath: "./tsconfig.json",
+        buildMode: true,
+      },
+      eslint: {
+        lintCommand: "eslint .",
+        useFlatConfig: true,
+        dev: {
+          overrideConfig: {
+            overrideConfigFile: "./eslint.config.js",
+          },
+        },
+      },
+    });
+
+    plugins.push(checkerPlugin);
+  }
+
+  return {
+    plugins,
+  };
 });
